dff8a74ce686e0183bf16f878ed091d683c0af49
pangpang20 <676814828@qq.com>
2025-08-08 23:23:44 +0800

    更新python310安装依赖

--- gaussdb-server/install_python310.sh
+++ gaussdb-server/install_python310.sh
@@ -1,6 +1,11 @@
 sudo yum groupinstall "Development Tools" -y
 sudo dnf install -y gcc gcc-c++ openssl-devel bzip2-devel libffi-devel zlib-devel wget make glibc-devel libgcc tar bzip2 zlib xz zx-devel readline-devel sqlite-devel sqlite-libs
 cd /usr/src
+
+if [ ! -f /lib64/libgcc_s.so.1 ]; then
+    sudo ln -sf /usr/lib64/libgcc_s.so.1 /lib64/libgcc_s.so.1
+fi
+
 sudo wget https://mirrors.huaweicloud.com/python/3.10.14/Python-3.10.14.tgz
 sudo tar xzf Python-3.10.14.tgz
 cd Python-3.10.14
